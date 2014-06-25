35850e643a9c484d61eedebeb9dea0ec4171818e
Adam Smith <adam.smith18@ntlworld.com>
2014-06-25 12:45:51 +0100

    Rough Help Screens

---
+++ src/info/adamjsmith/shrinkingsnake/HelpScreen3.java
@@ -0,0 +1,60 @@
+package info.adamjsmith.shrinkingsnake;
+
+import java.util.List;
+
+import info.adamjsmith.framework.Game;
+import info.adamjsmith.framework.Graphics;
+import info.adamjsmith.framework.Input.TouchEvent;
+import info.adamjsmith.framework.Screen;
+
+public class HelpScreen3 extends Screen {
+	public HelpScreen3(Game game) {
+		super(game);
+	}
+
+	@Override
+	public void update(float deltaTime) {
+		List < TouchEvent > touchEvents = game.getInput().getTouchEvents();
+		game.getInput().getKeyEvents();
+		
+		int len = touchEvents.size();
+		for(int i = 0; i < len; i++) {
+			TouchEvent event = touchEvents.get(i);
+			if(event.type == TouchEvent.TOUCH_UP) {
+				if(event.x > 630 && event.y > 1193) {
+					game.setScreen(new HelpScreen2(game));
+					if(Settings.soundEnabled)
+						Assets.click.play(1);
+					return;
+				}
+			}
+		}
+	}
+
+	@Override
+	public void present(float deltaTime) {
+		Graphics g = game.getGraphics();
+		g.drawPixmap(Assets.background, 0, 0);
+		g.drawPixmap(Assets.help1, 64, 100);
+		g.drawPixmap(Assets.buttons, 256, 416, 0, 64, 64, 64);		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
