dea11b4846cb6f5cfc86d288a3adbd47355eced2
Adam Smith <adam.smith18@ntlworld.com>
2014-06-25 10:29:09 +0100

    Initial commit

---
+++ src/info/adamjsmith/shrinkingsnake/ShrinkingSnakeGame.java
@@ -0,0 +1,15 @@
+package info.adamjsmith.shrinkingsnake;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ShrinkingSnakeGame extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
